85d65b545dfbff6eb835ffeb21c2c47ba3e4187e
vaughn18 <vaughn_mainacc@yahoo.com>
2020-12-02 09:10:11 +1300

    Identity added

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Job_Management_API.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Job_Management_API.Areas.Identity.IdentityHostingStartup))]
+namespace Job_Management_API.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Job_Management_APIContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Job_Management_APIContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<Job_Management_APIContext>();
+            });
+        }
+    }
+}
